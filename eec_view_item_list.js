4bbb8e7fd95c79031084d91f3d5475c8cc26b4bb
webozza <webozza@gmail.com>
2023-07-10 23:38:45 +0800

    version update

--- eec_view_item_list.js
+++ eec_view_item_list.js
@@ -129,7 +129,7 @@ jQuery(document).ready(function ($) {
       },
       ecommerce: {
         currency: "EUR",
-        items: productListing,
+        items: newProductListing,
       },
     });
   };
