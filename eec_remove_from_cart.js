e16e9d9ac9fe9be5f6ed404794b939b2ab0daadd
webozza <webozza@gmail.com>
2023-07-23 02:27:38 +0800

    version update

--- eec_remove_from_cart.js
+++ eec_remove_from_cart.js
@@ -5,9 +5,8 @@ jQuery(document).ready(function ($) {
     depId = $(this).data("departure-id");
   });
 
-  let removeFromCart = async () => {
+  let removeFromCart = () => {
     let removeFromCartItems = [];
-    let extaBedInitial = $('[name="is_on_extra_bed"]');
 
     if ($("body").hasClass("single-potovanja")) {
       oskarDepartures2.map((entries, index) => {
@@ -31,28 +30,6 @@ jQuery(document).ready(function ($) {
             travel_age_group: undefined,
             quantity: 1,
           });
-          console.log("checking...", extaBedInitial.is(":checked"));
-          if (extaBedInitial.is(":checked") == true) {
-            removeFromCartItems.push({
-              item_id: undefined,
-              item_name: "Želim bivati v sobi z dodatnim ležiščem",
-              item_brand: "Agencija Oskar",
-              item_category: "Travel",
-              item_category2: entries.country_name,
-              price: 0,
-              discount: entries.price - entries.actual_price,
-              affiliation: undefined,
-              travel_departure_date: entries.departure_start_date,
-              travel_style: entries.travel_style,
-              travel_type: undefined,
-              travel_group_size: entries.velikost_skupine,
-              travel_duration: entries.travel_duration,
-              travel_guide_id: undefined,
-              product_type: "Add-on",
-              travel_age_group: undefined,
-              quantity: 1,
-            });
-          }
         }
       });
     } else {
@@ -77,28 +54,6 @@ jQuery(document).ready(function ($) {
             travel_age_group: undefined,
             quantity: 1,
           });
-          console.log("checking...", extaBedInitial.is(":checked"));
-          if (extaBedInitial.is(":checked") == true) {
-            removeFromCartItems.push({
-              item_id: undefined,
-              item_name: "Želim bivati v sobi z dodatnim ležiščem",
-              item_brand: "Agencija Oskar",
-              item_category: "Travel",
-              item_category2: entries.country_name,
-              price: 0,
-              discount: entries.price - entries.actual_price,
-              affiliation: undefined,
-              travel_departure_date: entries.departure_start_date,
-              travel_style: entries.travel_style,
-              travel_type: undefined,
-              travel_group_size: entries.velikost_skupine,
-              travel_duration: entries.travel_duration,
-              travel_guide_id: undefined,
-              product_type: "Add-on",
-              travel_age_group: undefined,
-              quantity: 1,
-            });
-          }
         }
       });
     }
@@ -128,13 +83,15 @@ jQuery(document).ready(function ($) {
           formStatus = $(".application-form--content--inner--nav--item")
             .eq(0)
             .hasClass("active");
+          console.log(formStatus);
         });
 
-        $(".application-form-close").one("click", async function () {
+        $(".application-form-close").one("click", function () {
           if (formStatus == true) {
-            await removeFromCart();
+            removeFromCart();
+            console.log(formStatus);
           } else {
-            // do nothing
+            console.log(formStatus);
           }
         });
       }, 2000);
