8c0566070c1d0c92a8e00dd6b69715e4be2c1913
webozza <webozza@gmail.com>
2023-07-23 04:22:05 +0800

    items now contain all products

--- eec_add_payment_info copy.js
+++ eec_add_payment_info copy.js
@@ -27,7 +27,6 @@ jQuery(document).ready(function ($) {
           .prev()
           .text()
           .match(/\d+/)[0];
-        let totalQuantity = Number(adultQuantity) + Number(childQuanity) + 1;
 
         let checkedPaymentOption = $(this)[0].checked;
         if (checkedPaymentOption) {
@@ -48,27 +47,25 @@ jQuery(document).ready(function ($) {
           oskarDepartures2.map((entries, index) => {
             if (entries.ID == departureID) {
               cartPrice = entries.actual_price;
-              for (let i = 0; i < totalQuantity; i++) {
-                addPaymentInfo.push({
-                  item_id: entries.product_id,
-                  item_name: entries.travel_name,
-                  item_brand: "Agencija Oskar",
-                  item_category: "Travel",
-                  item_category2: entries.country_name,
-                  price: entries.actual_price,
-                  discount: entries.price - entries.actual_price,
-                  affiliation: undefined,
-                  travel_departure_date: entries.departure_start_date,
-                  travel_style: entries.travel_style,
-                  travel_type: undefined,
-                  travel_group_size: entries.velikost_skupine,
-                  travel_duration: entries.travel_duration,
-                  travel_guide_id: undefined,
-                  product_type: "Main",
-                  travel_age_group: undefined,
-                  quantity: Number(adultQuantity) + Number(childQuanity) + 1,
-                });
-              }
+              addPaymentInfo.push({
+                item_id: entries.product_id,
+                item_name: entries.travel_name,
+                item_brand: "Agencija Oskar",
+                item_category: "Travel",
+                item_category2: entries.country_name,
+                price: entries.actual_price,
+                discount: entries.price - entries.actual_price,
+                affiliation: undefined,
+                travel_departure_date: entries.departure_start_date,
+                travel_style: entries.travel_style,
+                travel_type: undefined,
+                travel_group_size: entries.velikost_skupine,
+                travel_duration: entries.travel_duration,
+                travel_guide_id: undefined,
+                product_type: "Main",
+                travel_age_group: undefined,
+                quantity: Number(adultQuantity) + Number(childQuanity) + 1,
+              });
               if (extraBedInitial.is(":checked")) {
                 addPaymentInfo.push({
                   item_id: undefined,
@@ -132,27 +129,25 @@ jQuery(document).ready(function ($) {
           oskarDepartures.map((entries, index) => {
             if (entries.ID == departureID) {
               cartPrice = entries.price;
-              for (let i = 0; i < totalQuantity; i++) {
-                addPaymentInfo.push({
-                  item_id: entries.product_id,
-                  item_name: entries.travel_name,
-                  item_brand: "Agencija Oskar",
-                  item_category: "Travel",
-                  item_category2: entries.country_name,
-                  price: entries.actual_price,
-                  discount: entries.price - entries.actual_price,
-                  affiliation: undefined,
-                  travel_departure_date: entries.departure_start_date,
-                  travel_style: entries.travel_style,
-                  travel_type: undefined,
-                  travel_group_size: entries.velikost_skupine,
-                  travel_duration: entries.travel_duration,
-                  travel_guide_id: undefined,
-                  product_type: "Main",
-                  travel_age_group: undefined,
-                  quantity: Number(adultQuantity) + Number(childQuanity) + 1,
-                });
-              }
+              addPaymentInfo.push({
+                item_id: entries.product_id,
+                item_name: entries.travel_name,
+                item_brand: "Agencija Oskar",
+                item_category: "Travel",
+                item_category2: entries.country_name,
+                price: entries.actual_price,
+                discount: entries.price - entries.actual_price,
+                affiliation: undefined,
+                travel_departure_date: entries.departure_start_date,
+                travel_style: entries.travel_style,
+                travel_type: undefined,
+                travel_group_size: entries.velikost_skupine,
+                travel_duration: entries.travel_duration,
+                travel_guide_id: undefined,
+                product_type: "Main",
+                travel_age_group: undefined,
+                quantity: Number(adultQuantity) + Number(childQuanity) + 1,
+              });
               if (extraBedInitial.is(":checked")) {
                 addPaymentInfo.push({
                   item_id: undefined,
