a59360f1b204dd1e66344b203f5b52090e50c08c
webozza <webozza@gmail.com>
2023-09-04 12:31:14 +0600

    update country template item list view eventg

--- eec_view_item_list.js
+++ eec_view_item_list.js
@@ -331,7 +331,7 @@ jQuery(document).ready(function ($) {
                 travel_guide_id: undefined,
                 product_type: "Main",
                 travel_age_group: undefined,
-                item_list_name: `${entries.travel_name}: Departures Table`,
+                item_list_name: `${item_category2}: Departures Table`,
                 //index: index + 1,
               });
             }
